5500492aefa2606cc3176f5339757144d65a4993
kamshushi <ahmed.elkamshoushy7@gmail.com>
2021-08-06 15:52:18 +0200

    2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import {ChatEngine} from 'react-chat-engine'
-import './App.css'
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
 
-const App = () =>{
-    return(
-        <ChatEngine 
-            height="100vh"
-            projectID=""
-            userName=""
-            userSecret=""
-        />  
-    )
-}
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="dc8a50e4-2043-4446-a80f-699d9d67ecbf"
+      userName="kamshushi"
+      userSecret="123456"
+    />
+  );
+};
 
 export default App;
